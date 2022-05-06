20fcfc99f2cdb2d7f6b2870707fe5a48bdea7f6c
Alexandre Lima <xandiie.ls@gmail.com>
2022-05-06 20:09:02 -0300

    tailwind config

---
+++ web/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      // criando uma cor padrão para o projeto
+      colors: {
+        brand: {
+          500: '#8257e6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
