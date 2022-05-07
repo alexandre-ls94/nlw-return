f540b6c27bf9fd483998ef64fb3b157ef9c4d61c
Alexandre Lima <xandiie.ls@gmail.com>
2022-05-07 17:22:24 -0300

    added plugins, create new default color and subscribe default rounded-md

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,10 +5,18 @@ module.exports = {
       // criando uma cor padrão para o projeto
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6'
         }
+      },
+      // sobrescreve o rounded-md que por padrão é 6px
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
