b9ff100c7bc3d9de76ab6a69a8f8bcf941655228
Alexandre Lima <xandiie.ls@gmail.com>
2022-05-08 15:17:11 -0300

    config jest and cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
 
 const port = 3000
 
+// o cors permite que a conexão entre frontend e backend exista
+// nesse caso está aberto para todas rotas de frontend, mas existem configurações para restringir acesso
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
